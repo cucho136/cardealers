b45ec1ef2d55ecb6aa603296b94d0a3d27154d0f
jesus herrera <jesus.herrera@mykukun.com>
2023-02-13 10:07:39 -0400

    Problem with update

---
+++ src/cars/cars.service.ts
@@ -0,0 +1,68 @@
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Car } from './interfaces/car.interface';
+import { v4 as uuid } from "uuid";
+import { CreateCarDto, UpdateCarDto } from './dto';
+
+@Injectable()
+export class CarsService {
+    private cars : Car[] = [
+        {
+            id: uuid(),
+            brand:'Toyota',
+            model: 'Corolla'
+        },
+        {
+            id: uuid(),
+            brand:'Honda',
+            model: 'Civic'
+        },
+        {
+            id: uuid(),
+            brand:'Jeep',
+            model: 'Cherokee'
+        },
+    ];
+
+    findAll(){
+        return this.cars;
+    }
+
+    findOneById( id: string){
+        const car = this.cars.find( car  => car.id ===id)
+        if (!car) throw new NotFoundException(`Car with id '${ id } not found'`);
+        return { car }
+    }
+
+    create ( createCarDto:CreateCarDto){
+        const newCar: Car ={
+            id: uuid(),
+            brand: createCarDto.brand,
+            model: createCarDto.model
+        }
+
+        this.cars.push(newCar)
+
+        return newCar
+    }
+
+    update(id: string, updateCarDto:UpdateCarDto){
+
+        let carDB = this.findOneById(id);
+
+        if( updateCarDto.id && updateCarDto!== id){
+            throw new BadRequestException(`car id is not valid inside body`);
+        }
+
+        this.cars = this.cars.map( car => {
+            if (car.id === id){
+                carDB = {...carDB, ...updateCarDto, id}
+                
+                return carDB
+            }
+
+            return car;
+        })
+
+        return carDB;  
+    }
+}
